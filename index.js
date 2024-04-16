57469076c72487d16a18d3ed4ef776a5d70d1bea
luana castro <167257323+lulucastro@users.noreply.github.com>
2024-04-16 20:51:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12) {
+        // BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = ('#c27713')
+
+    } else if (hora >=13 && hora <= 18) {
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = ('#8e3d27')
+    } else {
+        //BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = ('#032e64')
+    }
+
+}
+
